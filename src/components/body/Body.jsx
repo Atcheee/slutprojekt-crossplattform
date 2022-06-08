bf4d56bbacd1a179a7c61d403a7b75ac946c4667
Atche <erik90781@gmail.com>
2022-06-08 16:13:28 +0200

    Dog image fetch added, styling almost done

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import useFetchWeather from "../../api/fetchWeather";
 import useFetchDepartures from "../../api/fetchDepartures";
+import useFetchDogs from "../../api/fetchDogs";
 import Clock from "./clock/Clock";
+import Style from "./body.module.css";
 
 function Body() {
   const { data: weather } = useFetchWeather(
@@ -10,16 +12,31 @@ function Body() {
   const { data: departure } = useFetchDepartures(
     "https://api.resrobot.se/v2.1/departureBoard?id=740000002&format=json&accessId=644710bc-1d39-4371-b149-6b7a6f0c9cdd"
   );
+  const { data: dog } = useFetchDogs("https://dog.ceo/api/breeds/image/random");
+
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
 
   return (
-    <div>
-      <h1><Clock /></h1>
-      <h1> Country: {weather && weather.name} </h1>
-      <h1>
-        {" "}
-        Weather Description: {weather && weather.weather[0].description}{" "}
-      </h1>
-      <h1> Temp: {weather && Math.floor(weather.main.temp - 273.15)} </h1>
+    <div className={Style.wrapper}>
+      <div className={Style.clock}>
+        <h1 className={Style.clock_timer}>
+          <Clock />
+        </h1>
+      </div>
+      <div className={Style.weather_container}>
+        <h1> City: {weather && weather.name} </h1>
+        <h1>
+          {" "}
+          Weather Description: {weather && weather.weather[0].description}{" "}
+        </h1>
+        <h1> Temp: {weather && Math.floor(weather.main.temp - 273.15)}° </h1>
+      </div>
+      <div className={Style.dog_images}>
+        <img src={dog && dog.message} alt="Random dog" />
+      </div>
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import style from "./header.module.css";
+import IthsLogo from '../../assets/iths-logo.png'
 
 const Header = () => {
   return (
     <div className={style.wrapper}>
+      <img src={IthsLogo} alt="" />
     </div>
   );
 };
