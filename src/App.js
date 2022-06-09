95409bb5a2f48adf62832172f0f6a59a080ce34b
Nabhan Abdullah <nabhan.abdullah@iths.se>
2022-06-09 11:06:01 +0200

    online offline netfily

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import Header from "./components/header/Header";
 import Body from "./components/body/Body";
 import Footer from "./components/footer/Footer";
 
 function App() {
+  const [onlineState, setOnlineState] = useState(true);
+
+  useEffect(() => {
+    const showOffline = () => {
+      setOnlineState(false);
+    };
+    const showOnline = () => {
+      setOnlineState(true);
+    };
+
+    window.addEventListener("offline", showOffline);
+    window.addEventListener("online", showOnline);
+
+    return () => {
+      window.removeEventListener("offline", showOffline);
+      window.removeEventListener("online", showOnline);
+    };
+  }, []);
   return (
     <div className="App">
       <div>
-        <Header />
+        <Header onlineState={onlineState} />
         <Body />
         <Footer />
       </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from "./header.module.css";
-import IthsLogo from '../../assets/iths-logo.png'
+import IthsLogo from "../../assets/iths-logo.png";
 
-const Header = () => {
+const Header = ({ onlineState }) => {
   return (
     <div className={style.wrapper}>
+      <h3>{onlineState ? "Your are online" : "You are offline"} </h3>
       <img src={IthsLogo} alt="" />
     </div>
   );
